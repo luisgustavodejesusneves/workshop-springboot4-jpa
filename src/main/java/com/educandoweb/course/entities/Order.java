693f3f24a7eea5902e13b02a3f1c851a0b60644c
Luis Gustavo <luisgustavo.email10@gmail.com>
2026-03-05 10:29:11 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -30,7 +30,7 @@ public class Order implements Serializable{
 	private Integer orderStatus;
 	
 	@ManyToOne
-	@JoinColumn(name = "client_di")
+	@JoinColumn(name = "client_id")
 	private User client;
 	
 	public Order() {
